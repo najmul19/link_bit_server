3a22151882cc3b798763b038954e0034501ca519
najmul <mdnajmulislam10992@gmail.com>
2026-01-09 21:06:43 +0600

    console for checking vercel prblem

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const urlRoutes = require('./routes/urlRoutes');
-const redirectRoutes = require('./routes/redirectRoutes');
+const urlRoutes = require("./routes/urlRoutes");
+const redirectRoutes = require("./routes/redirectRoutes");
 
 const app = express();
+console.log(
+  "MONGODB_URI on Vercel (prefix):",
+  (process.env.MONGODB_URI || "").slice(0, 60)
+);
 
 if (!mongoose.connection.readyState) {
   mongoose
     .connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB error', err));
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB error", err));
 }
 
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
+    credentials: true,
   })
 );
 app.use(express.json());
 
-app.use('/api/urls', urlRoutes);
-app.use('/r', redirectRoutes);
+app.use("/api/urls", urlRoutes);
+app.use("/r", redirectRoutes);
 
-app.get('/', (req, res) => {
-  res.send('URL Shortener API running');
+app.get("/", (req, res) => {
+  res.send("URL Shortener API running");
 });
 
 module.exports = app;
